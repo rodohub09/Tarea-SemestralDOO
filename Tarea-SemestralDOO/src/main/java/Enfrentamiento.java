ca50c2b4147e926f80baae33f8f9b6f472d4f32b
mihn <martin.henrnav@gmail.com>
2025-06-29 22:11:35 -0400

    Crear variable booleana esEmpate, modificar el método setGanador actualizando los datos de los participantes enfrentados y crear el método setEmpate con una estructura similar a setGanador, solo que con ganador nulo. Creación de un getter para esEmpate

--- Tarea-SemestralDOO/src/main/java/Enfrentamiento.java
+++ Tarea-SemestralDOO/src/main/java/Enfrentamiento.java
@@ -4,20 +4,37 @@ public class Enfrentamiento {
     private Participante local;
     private Participante visita;
     private Participante ganador = null;
+    private boolean esEmpate = false;
     private Date fecha;
 
     //Quizás se puede añadir el equipo perdedor también para los torneos de doble eliminación y las ligas
-    public Enfrentamiento(Participante local, Participante visita){
+    public Enfrentamiento(Participante local, Participante visita) {
         this.local = local;
         this.visita = visita;
     }
 
     public void setGanador(Participante ganador) {
         this.ganador = ganador;
+        this.esEmpate = false;
+
+        local.registrarResultado(ganador, (local == ganador) ? Resultados.Victoria : Resultados.Derrota);
+        visita.registrarResultado(ganador, (visita == ganador) ? Resultados.Victoria : Resultados.Derrota);
+    }
+
+    public void setEmpate() {
+        this.ganador = null;
+        this.esEmpate = true;
+
+        local.registrarResultado(null, Resultados.Empate);
+        visita.registrarResultado(null, Resultados.Empate);
     }
 
     public Participante getGanador() {
         return ganador;
+    }
+
+    public boolean isEmpate() {
+        return esEmpate;
     }
 
     @Override
