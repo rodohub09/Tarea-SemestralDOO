5d8efe3730d33110eab662b2e72d8f8c32104c25
mihn <martin.henrnav@gmail.com>
2025-07-12 21:52:34 -0400

    Eliminar clase Grupos y eliminar codigo relacionado a esta clase en Participantes

--- Tarea-SemestralDOO/src/main/java/modelos/Participantes.java
+++ Tarea-SemestralDOO/src/main/java/modelos/Participantes.java
@@ -12,7 +12,6 @@ public class Participantes {
     public void setAgrupacionParticipantes(TipoTorneo tipoTorneo){
         switch (tipoTorneo){
             case Liga -> agrupacionParticipantes = new Clasificacion();
-            case FaseGrupos -> agrupacionParticipantes = new Grupos();
             case EliminacionDirecta -> agrupacionParticipantes = new Bracket();
         }
     }
