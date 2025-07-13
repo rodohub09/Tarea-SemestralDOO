d6848b5fc152ed839fee010dc616224356d7e693
mihn <martin.henrnav@gmail.com>
2025-07-13 19:03:13 -0400

    Creacion de metodo actualizarParticipantes en Bracket para ajustar la lista de participantes segun avanzan las rondas

--- Tarea-SemestralDOO/src/main/java/modelos/Bracket.java
+++ Tarea-SemestralDOO/src/main/java/modelos/Bracket.java
@@ -1,30 +1,45 @@
 package modelos;
 import java.util.ArrayList;
 
-public class Bracket implements AgrupacionParticipantes{
+public class Bracket implements AgrupacionParticipantes {
     private ArrayList<Participante> bracket1;
     private ArrayList<Participante> bracket2;
+    private ArrayList<Participante> participantesActivos;
 
     public Bracket(){
         this.bracket1 = new ArrayList<>();
         this.bracket2 = new ArrayList<>();
+        this.participantesActivos = new ArrayList<>();
     }
 
     @Override
     public void organizarParticipantes(ArrayList<Participante> participantes) {
-        for(int i = 0; i < participantes.size()/2; i++){
-            bracket1.add(participantes.get(i));
-            bracket2.add(participantes.get(participantes.size()-(i+1)));
+        for(int i = 0; i < participantes.size() / 2; i++){
+            Participante p1 = participantes.get(i);
+            Participante p2 = participantes.get(participantes.size() - i - 1);
+            bracket1.add(p1);
+            bracket2.add(p2);
         }
+        participantesActivos.addAll(participantes);
+    }
+
+    public void actualizarParticipantes(ArrayList<Enfrentamiento> enfrentamientos) {
+        ArrayList<Participante> activos = new ArrayList<>();
+        for (Enfrentamiento e : enfrentamientos) {
+            Participante ganador = e.getGanador();
+            if (ganador != null)
+                activos.add(ganador);
+        }
+        this.participantesActivos = activos;
     }
 
     @Override
     public ArrayList<Participante> devolverAgrupacion() {
-        return null;
+        return new ArrayList<>(participantesActivos);
     }
 
     @Override
     public String toString() {
-        return bracket1.toString() + bracket2.toString();
+        return bracket1.toString() + bracket2.toString() + participantesActivos.toString();
     }
 }
