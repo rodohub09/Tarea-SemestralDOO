641c20b301494097cc37e6cf6dba414f3c078825
mihn <martin.henrnav@gmail.com>
2025-07-13 23:42:31 -0400

    Documentacion en interfaces, enums y excepciones

--- Tarea-SemestralDOO/src/main/java/modelos/CantidadInscritosInvalidaException.java
+++ Tarea-SemestralDOO/src/main/java/modelos/CantidadInscritosInvalidaException.java
@@ -1,6 +1,13 @@
 package modelos;
 
+/**
+ * Excepción que se lanza cuando se quiere crear un torneo y la cantidad de participantes ingresados
+ * no coincide con la cantidad de participantes seleccionada.
+ */
 public class CantidadInscritosInvalidaException extends RuntimeException {
+    /**
+     * Crea una instancia de la excepción {@code CantidadInscritosInvalidaException}.
+     */
     public CantidadInscritosInvalidaException() {
         super();
     }
--- Tarea-SemestralDOO/src/main/java/modelos/TipoDeCalendario.java
+++ Tarea-SemestralDOO/src/main/java/modelos/TipoDeCalendario.java
@@ -1,7 +1,26 @@
 package modelos;
+
 import java.util.ArrayList;
 
+/**
+ * Interfaz que define el comportamiento de un calendario de torneo.
+ * Permite la creación y actualización de jornadas según el tipo de torneo (liga, eliminación directa, eliminacion doble).
+ */
 interface TipoDeCalendario {
+
+    /**
+     * Crea el calendario inicial del torneo, generando las jornadas y enfrentamientos correspondientes.
+     * @param alj Lista de jornadas donde se agregará la estructura del calendario.
+     * @param cantEnfretamientosIgual Cantidad de veces que se enfrentan los mismos participantes
+     * @param p Objeto que contiene información sobre la cantidad de participantes y jornadas.
+     * @param participantes Lista de participantes del torneo.
+     */
     void creacionCalendario(ArrayList<Jornada> alj, int cantEnfretamientosIgual, CantidadParticipantes p, Participantes participantes);
+
+    /**
+     * Actualiza el calendario después de una jornada completada, agregando nuevos enfrentamientos si es necesario.
+     * @param alj Lista de jornadas del torneo.
+     * @param cantEnfretamientosIgual Cantidad de veces que se enfrentan los mismos participantes.
+     */
     void actualizarCalendario(ArrayList<Jornada> alj, int cantEnfretamientosIgual);
 }
