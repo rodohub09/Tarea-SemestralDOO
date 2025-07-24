e06f0e010f4fe9ab523990e842379d1683d7d789
mihn <martin.henrnav@gmail.com>
2025-07-24 18:11:45 -0400

    Eliminar clases no utilizadas y eliminar variables innecesarias en CalendarioElimDirecta

--- Tarea-SemestralDOO/src/main/java/modelos/CalendarioElimDirecta.java
+++ Tarea-SemestralDOO/src/main/java/modelos/CalendarioElimDirecta.java
@@ -6,9 +6,6 @@ import java.util.ArrayList;
  * Se encarga de generar las jornadas a partir de los participantes activos proporcionados por la agrupación.
  */
 public class CalendarioElimDirecta implements TipoDeCalendario {
-    public int fecha = 0;
-    public static String tipoCalendario = "Eliminacion Directa";
-
     /**
      * Crea las jornadas iniciales de un torneo de eliminación directa.
      * @param alj Lista donde se agregan las jornadas generadas.
@@ -18,14 +15,13 @@ public class CalendarioElimDirecta implements TipoDeCalendario {
      */
     @Override
     public void creacionCalendario(ArrayList<Jornada> alj, int cantEnfretamientosIgual, CantidadParticipantes p, Participantes participante) {
-        Jornada jornadaInicial = new Jornada(participante);
+        Jornada jornadaInicial = new Jornada();
         int n = participante.participantes.size();
         for (int i = 0; i < n / 2; i++) {
             Enfrentamiento ida = new Enfrentamiento(participante.participantes.get(i), participante.participantes.get(n - i - 1), null);
             jornadaInicial.getEnfrentamientos().add(ida);
         }
         alj.add(jornadaInicial);
-        this.fecha += cantEnfretamientosIgual;
     }
 
     /**
@@ -35,13 +31,12 @@ public class CalendarioElimDirecta implements TipoDeCalendario {
      * @param activos Lista de participantes activos entregados por la agrupación.
      */
     public void actualizarCalendario(ArrayList<Jornada> alj, int cantEnfretamientosIgual, ArrayList<Participante> activos) {
-        Jornada nuevaJornada = new Jornada(new Participantes());
+        Jornada nuevaJornada = new Jornada();
         int n = activos.size();
         for (int i = 0; i < n / 2; i++) {
-            Enfrentamiento ida = new Enfrentamiento(activos.get(i), activos.get(n - i - 1), null);
-            nuevaJornada.getEnfrentamientos().add(ida);
+            Enfrentamiento e = new Enfrentamiento(activos.get(i), activos.get(n - i - 1), null);
+            nuevaJornada.getEnfrentamientos().add(e);
         }
         alj.add(nuevaJornada);
-        this.fecha += cantEnfretamientosIgual;
     }
 }
