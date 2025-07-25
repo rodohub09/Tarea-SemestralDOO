0cf41695fa1dde30a26c46648177f2836850b8da
rodohub09 <superpro757@gmail.com>
2025-07-24 23:21:10 -0400

    Se agrega un borde a PanelEnfrentamiento para mayor claridad al momento de visualizarlo en pantalla

--- Tarea-SemestralDOO/src/main/java/vistas/PanelEnfrentamiento.java
+++ Tarea-SemestralDOO/src/main/java/vistas/PanelEnfrentamiento.java
@@ -1,6 +1,8 @@
 package vistas;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class PanelEnfrentamiento extends JPanel {
     private JLabel labelLocal;
@@ -10,17 +12,18 @@ public class PanelEnfrentamiento extends JPanel {
         super();
         setSize(80,60);
         setLayout(null);
+        setBorder(new LineBorder(Color.BLACK,1));
         this.labelLocal = new JLabel(local);
         this.labelVisita = new JLabel(visita);
         this.labelvs = new JLabel("vs.");
 
-        labelLocal.setBounds(0,0,80,20);
+        labelLocal.setBounds(2,1,80,20);
         labelLocal.setFont(super.getFont().deriveFont(16f));
 
         labelvs.setBounds(32,20,40,20);
         labelvs.setFont(super.getFont().deriveFont(16f));
 
-        labelVisita.setBounds(0,40,80,20);
+        labelVisita.setBounds(2,40,80,20);
         labelVisita.setFont(super.getFont().deriveFont(16f));
 
         add(labelLocal);
